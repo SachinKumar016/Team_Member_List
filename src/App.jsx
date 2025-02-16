8b4a649fa5e7afc466eab438f9b6d70a019639e6
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-02-16 23:22:44 +0530

    zero commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
 import "./App.css";
 
 
 function App() {
 
   return(
-    <nav className="container bg-blue-300 p-4 flex">
+    <nav className="w-7xl m-0  container bg-blue-300 p-4 flex">
       <h1 className="font-bold text-amber-600" id="nav">Page</h1>
-      <ul className="flex end-1 gap-9">
+      <ul className="cal">
         <li className="text-purple-900">Explore</li>
-        <li>About</li>
-        <li>Product</li>
-        <li>Contact</li>
+        <li className="text-purple-900">About</li>
+        <li className="text-purple-900">Product</li>
+        <li className="text-purple-900">Contact</li>
       </ul>
     </nav>
   )
