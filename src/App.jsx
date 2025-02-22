8deb8a832d5c74162b0333dc83909385de70462d
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-02-22 23:42:48 +0530

    2nd commit
    Some work with three.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import Nav from './Nav';
 function App() {
   return (
     <div className=" top-0 left-0 w-full">
-  <Nav />
-    <div className="min-h-100vh bg-blue-300 p-7 mt-28">
-      <h1 className="text-2xl font-bold">My React App</h1>
-      
-    </div>
+      <Nav />
     </div>
   );
 }
