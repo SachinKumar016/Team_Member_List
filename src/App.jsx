01886ef01573ee1d33e48ab2a902366b0e73aec1
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-02-24 23:52:01 +0530

    2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from './Nav';
+import ThreeJSComponent from './canva.jsx';
 
 function App() {
   return (
     <div className=" top-0 left-0 w-full">
       <Nav />
+      <ThreeJSComponent />
     </div>
   );
 }
