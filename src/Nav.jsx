6fcbf7e7015dff0af54468b8721b4e6116b1e98e
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-02-26 23:57:34 +0530

    2.2

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,13 @@
 
 function nav() {
   return(
-    <nav className="fixed top-0 left-0 w-full bg-blue-300 p-4 flex justify-between items-center shadow-md">
-      <h1 className="font-bold text-amber-600 text-xl">Page</h1>
-      <ul className="flex space-x-6 bg-amber-600 px-4 py-2 rounded-md">
-        <li className="text-purple-900 cursor-pointer">Explore</li>
-        <li className="text-purple-900 cursor-pointer">About</li>
-        <li className="text-purple-900 cursor-pointer">Product</li>
-        <li className="text-purple-900 cursor-pointer">Contact</li>
+    <nav className="fixed top-0 left-0 w-full bg-transparent p-4 flex justify-center items-center shadow-md">
+      <h1 className="font-bold text-white text-xl absolute left-4">Page</h1>
+      <ul className="flex space-x-25 px-4 py-2 rounded-md place-self-center">
+        <li className="text-white cursor-pointer">Explore</li>
+        <li className="text-white cursor-pointer">About</li>
+        <li className="text-white cursor-pointer">Product</li>
+        <li className="text-white cursor-pointer">Contact</li>
       </ul>
     </nav>
   )
