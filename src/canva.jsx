2fda92dcd145de480e2489f0df8ee75531cde2c5
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-02-27 23:57:14 +0530

    2.3

--- src/canva.jsx
+++ src/canva.jsx
@@ -1,9 +1,18 @@
-function canva() {
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import React from "react";
+
+function CanvasComponent() {
   return (
-    <Canvas>
-
-    </Canvas>
+   <Canvas className="mt-50 ml-40" style={{ width: "400px", height: "400px" }} camera={{ position: [3, 3, 3] }}>
+    <OrbitControls />
+    <ambientLight />
+    <mesh>
+      <boxGeometry args={[1,1,1,30,30, false]}/>
+      <meshBasicMaterial />
+    </mesh>
+   </Canvas>
   );
-};
+}
 
-export default canva;
+export default CanvasComponent;
