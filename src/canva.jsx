0d7ff8149190db0913378901f69baf395fe980c9
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-02-28 23:53:35 +0530

    3 commit

--- src/canva.jsx
+++ src/canva.jsx
@@ -1,15 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import React from "react";
+import * as THREE from "three";
 
 function CanvasComponent() {
   return (
-   <Canvas className="mt-50 ml-40" style={{ width: "400px", height: "400px" }} camera={{ position: [3, 3, 3] }}>
+   <Canvas style={{ width: "100%", height: "70vh", background:"blue" }} camera={{ position: [3, 3, 3] }}>
     <OrbitControls />
     <ambientLight />
-    <mesh>
-      <boxGeometry args={[1,1,1,30,30, false]}/>
-      <meshBasicMaterial />
+    <mesh position={[3,2,3]}>
+      <sphereGeometry args={[ true]}/>
+      <meshBasicMaterial side={THREE.DoubleSide} />
     </mesh>
    </Canvas>
   );
