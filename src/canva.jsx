8e99cc1c450eb5a062aba6a9d8799804e3591276
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-03-06 23:49:23 +0530

    3.2

--- src/canva.jsx
+++ src/canva.jsx
@@ -5,14 +5,7 @@ import * as THREE from "three";
 
 function CanvasComponent() {
   return (
-   <Canvas style={{ width: "100vw", height: "90vh", background:"blue" }} camera={{ position: [0, 0, 0] }} scene={1000}>
-    <OrbitControls />
-    <ambientLight />
-    <mesh position={new THREE.Vector3(0, -50, -20)}>
-      <sphereGeometry args={[49, 50,50,true]}/>
-      <meshBasicMaterial side={THREE.DoubleSide} />
-    </mesh>
-   </Canvas>
+   
   );
 }
 
