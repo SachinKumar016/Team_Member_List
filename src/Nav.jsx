37e30891baeb2a0a9c21be622925ac3bab5fd709
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-03-07 23:54:11 +0530

    3.5

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,7 @@
 
 function nav() {
   return(
-    <nav className="fixed top-0 left-0 w-full bg-transparent p-4 flex justify-center items-center shadow-md">
+    <nav className="fixed top-0 left-0 w-full bg-transparent p-4 flex justify-center items-center">
       <h1 className="font-bold text-white text-xl absolute left-4">Page</h1>
       <ul className="flex space-x-25 px-4 py-2 rounded-md place-self-center">
         <li className="text-white cursor-pointer">Explore</li>
