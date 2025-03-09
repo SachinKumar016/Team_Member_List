16b0646fb707785bd60ecb4e0a0a11e9561dd598
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-03-09 23:35:15 +0530

    4th commit

--- src/canva.jsx
+++ src/canva.jsx
@@ -5,7 +5,10 @@ import * as THREE from "three";
 
 function CanvasComponent() {
   return (
-   
+   <div>
+    <img src="D:\HTML\Team_Member_List\Team_Member_List\5e9a56b849759f7dbed019d658cb17a1-removebg-preview.png" className=""/>
+    
+   </div>
   );
 }
 
