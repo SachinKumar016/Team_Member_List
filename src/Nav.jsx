c98c6d5ebdb8b6f4170c193d160729bc182b7ea6
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-04-04 20:32:33 +0530

    11th commit

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,14 +4,14 @@ function nav() {
     <nav className="fixed top-0 left-0 w-full bg-transparent p-4 flex justify-center items-center">
       <h1 className="font-bold text-white text-2xl absolute left-10 top-3">Paradox</h1>
       <ul className="list">
-        <li className="text-white cursor-pointer">Home</li>
-        <li className="text-white cursor-pointer">Explore</li>
-        <li className="text-white cursor-pointer">About</li>
+        <li className="a1">Home</li>
+        <li className="a2">Explore</li>
+        <li className="a3">About</li>
         <div className='cv'></div>
         <div className="at">
-        <div className="ab"></div>
-        <div className="ab"></div>
-        <div className="ab"></div>
+            <div className="ab"></div>
+            <div className="ab"></div>
+            <div className="ab"></div>
         </div>
       </ul>
     </nav>
--- src/canva.jsx
+++ src/canva.jsx
@@ -1,10 +1,13 @@
 import './canva.css'
 function Canvas() {
-    <>
+    return(
     <div>
+        <div className="circle"></div>
+        <div className="front">Pick up Fast</div>
+        <button>Explore</button>
 
     </div>
-    </>
+    );
 }
 
 export default Canvas
