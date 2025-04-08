24db3c20c6bf441555a41a1153f2a94df03de207
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-04-08 23:57:06 +0530

    13th commit

--- src/Team.jsx
+++ src/Team.jsx
@@ -3,6 +3,12 @@ import './Team.css'
 function Team() {
     return(
         <div className="Team">
+            <h2 className="template">TEMPLATE</h2>
+            <div className="product1">
+            <div className="info1">
+                <div className="price1"></div>
+            </div>
+            </div>
         </div>
     );
 }
