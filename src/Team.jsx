a60780a5c70f29e12c93816d170a0bcb30e3b7b2
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-04-21 23:55:39 +0530

    16th commit

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,12 +1,26 @@
 import './Team.css'
 
+
 function Team() {
     return(
         <div className="Team">
             <h2 className="template">TEMPLATE</h2>
             <div className="product1">
+                <h2 className='notice'>Yet to come</h2>
                 <div className="info1">
                     <div className="price1"></div>
+                </div>
+            </div>
+            <div className="product2">
+                <h2 className='notice1'>Yet to come</h2>
+                <div className="info2">
+                    <div className="price2"></div>
+                </div>
+            </div>
+            <div className="product3">
+                <h2 className='notice1'>Yet to come</h2>
+                <div className="info2">
+                    <div className="price2"></div>
                 </div>
             </div>
         </div>
